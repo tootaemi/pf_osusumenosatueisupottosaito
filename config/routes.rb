25186735c676803e247d6bb2db5084bcdf75afa1
tootaemi <n4mtkk@gmail.com>
2023-05-22 14:05:47 +0000

    [Update]ゲストログイン途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,38 @@
 Rails.application.routes.draw do
-    root to: 'homes#top'
+  get 'homes/top'
+  
+  # devise_scope :user do
+  # end
+  
+  # devise_scope :customer do
+    get "customers/guest_sign_in", to: "customers/sessions#guest_sign_in"
+    get 'customers/sign_out' => 'devise/sessions#destroy'
+    get 'customers', to: redirect("customers/sign_up")
+    post 'customers/guest_sign_in', to: 'customers/sessions#guest_sign_in'
+  # end
+  
+  # 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "customer/registrations",
+  sessions: 'customer/sessions'
+}
 
-  devise_for :customers
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  
+  
+    # root to: 'homes#top'
+  
 
-#
+
+  # scope module: :public do
+  #   root 'homes#top'
+  # end
+
 
   #resources :customers
 

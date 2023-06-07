b17ab91c2783fc4d30171fa60f1e355f1d8468cf
tootaemi <n4mtkk@gmail.com>
2023-06-07 14:33:14 +0000

    [Update]ログインOk

--- app/controllers/customer/sessions_controller.rb
+++ app/controllers/customer/sessions_controller.rb
@@ -7,7 +7,7 @@ class Customer::SessionsController < Devise::SessionsController
   #   redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
   # end
 
-  before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # ログイン後のリダイレクト先
   # def after_sign_in_path_for(resource_or_scope)
@@ -16,7 +16,7 @@ class Customer::SessionsController < Devise::SessionsController
 
   # ログアウト後のリダイレクト先
   def after_sign_out_path_for(resource_or_scope)
-    root_path 
+    root_path
   end
 
 
@@ -26,7 +26,6 @@ class Customer::SessionsController < Devise::SessionsController
     redirect_to root_path, notice: "ゲストユーザーとしてログインしました"
   end
 
-  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -34,19 +33,23 @@ class Customer::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    p '-------create-----'
+    p params
+    p configure_sign_in_params
+    p '------------'
+    super
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,72 +1,66 @@
 Rails.application.routes.draw do
-
-          # resources :customer
-          # namespace :customer do
-
-          #   resources :posts
-          #   # post "/posts" => "/customer/posts#index"
-          #   # get 'posts/:id', to: 'posts#show'
-          #     # get 'posts/:id' => 'posts#show'
-          #   end
-
+  # resources :customer
+  # namespace :customer do
+  #   resources :posts
+  #   # post "/posts" => "/customer/posts#index"
+  #   # get 'posts/:id', to: 'posts#show'
+  #     # get 'posts/:id' => 'posts#show'
+  #   end
   # devise_for :customers, :controllers => {
   #   :registrations => 'users/registrations',
   #   :sessions => 'users/sessions',
   # }
-     devise_scope :customer do
-    get '/customer/sign_out' => 'customer/sessions#destroy'
-    get '/customer', to: redirect("/customer/sign_up")
-    #get 'customer/guest_sign_up', to: 'customers/sessions#new_guest'
-    post 'customer/guest_sign_in', to: 'customer/sessions#new_guest'
-
-#get 'customer/guest_sign_in',to: 'customers/registrations#new'
-    #get 'customer/guest_sign_up', to: 'customers/sessions#new'
-
-    get "customer/sign_up" => "customer/registrations#new"
-    delete "customer/sign_out"  => "customer/sessions#destroy"
-
-  end
-       scope module: :customer do
-        post "/" => "homes#top"
-        root to: 'homes#top'
-        
-         post "customer" => "customers#create"
-         post "customer" => "customers#show"
-
-         get "customer/edit" => "customers#edit"
-         #patch  "customer/edit" => "customers#edit"
-         patch "customer" => "customers#update"
-           # root to: 'posts#index'
-  resources :posts, except: %w[index]
-          post "post/:id" => "posts#show"
-          patch "post/id" => "posts#update"
-          
-  resources :tags, only: %w[index show destroy]
-
-
-        resources :customers
-        get "customer" => "customers#show"
-        
-        get "post/new" => "posts#new"
-        post "post/new" => "posts#new"
-
-        # patch "posts/:id" => "posts#update"
-
-        # post "post/:id" => "post#show"
-
-        # resources :post
-
+  #     devise_scope :customer do
+  #     get '/customer/sign_out' => 'customer/sessions#destroy'
+  #     get '/customer', to: redirect("/customer/sign_up")
+  #     #get 'customer/guest_sign_up', to: 'customers/sessions#new_guest'
+  #     post 'customer/guest_sign_in', to: 'customer/sessions#new_guest'
+  # #get 'customer/guest_sign_in',to: 'customers/registrations#new'
+  #     #get 'customer/guest_sign_up', to: 'customers/sessions#new'
+  #     get "customer/sign_up" => "customer/registrations#new"
+  #     delete "customer/sign_out"  => "customer/sessions#destroy"
+  #   end
+  # 顧客用
+  # URL /customers/sign_in ...
+  devise_for :customer,skip: [:passwords], controllers: {
+    registrations: "customer/registrations",
+    sessions: 'customer/sessions'
+  }
+  # # 管理者用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  scope module: :customer do
+    root to: 'homes#top'
+    post "/" => "homes#top"
+    # post "customer" => "customers#create"
+    post "customer" => "customers#show"
+    get "customer/edit" => "customers#edit"
+    #patch  "customer/edit" => "customers#edit"
+    patch "customer" => "customers#update"
+    # root to: 'posts#index'
+    resources :posts, except: %w[index]
+    post "post/:id" => "posts#show"
+    resources :tags, only: %w[index show destroy]
+    resources :customers
+    get "customer" => "customers#show"
+    get "post/new" => "posts#new"
+    post "post/new" => "posts#new"
+    patch "posts/id" => "posts#update"
+    # patch "posts/:id" => "posts#update"
+    # post "post/:id" => "post#show"
+    # resources :post
     resources :customers, only: [:index, :new, :create ,:show ,:update, :edit, :destroy] do
-    post 'tag/:id' => 'tags#create', as: 'tag'
-    delete 'tag/:id' => 'tags#destroy', as: 'unlike'
-    resources :comments, only: [:create, :destroy]
-    collection do
-      get 'search'
+      post 'tag/:id' => 'tags#create', as: 'tag'
+      delete 'tag/:id' => 'tags#destroy', as: 'unlike'
+      resources :comments, only: [:create, :destroy]
+      collection do
+        get 'search'
+      end
     end
   end
 end
-
-
 
 
  # resources :users do
@@ -115,26 +109,8 @@ end
 
 
 
-  # 顧客用
-# URL /customers/sign_in ...
-devise_for :customer,skip: [:passwords], controllers: {
-  registrations: "customer/registrations",
-  sessions: 'customer/sessions'
 
-
-
-
-}
-
-# # 管理者用
-# URL /admin/sign_in ...
-devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
 
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
-
-
